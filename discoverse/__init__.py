d89ee9432e2f5dc5a6a088cf9ed9ddc0bca36afe
jyf23 <1305116641@qq.com>
2025-02-13 18:58:37 +0800

    update DISCOVERSE_ASSERT_DIR

--- discoverse/__init__.py
+++ discoverse/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 DISCOVERSE_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-if os.getenv('DISCOVERSE_ROOT_DIR'):
-    DISCOVERSE_ASSERT_DIR = os.getenv('DISCOVERSE_ROOT_DIR')
+if os.getenv('DISCOVERSE_ASSERT_DIR'):
+    DISCOVERSE_ASSERT_DIR = os.getenv('DISCOVERSE_ASSERT_DIR')
     print(f'>>> get env "DISCOVERSE_ASSERT_DIR": {DISCOVERSE_ASSERT_DIR}')
 else:
     DISCOVERSE_ASSERT_DIR = os.path.join(DISCOVERSE_ROOT_DIR, 'models')
