5e5ce0c2546a0d660a3a4162bb52b8823df66b21
Beiwen Tian <tianbeiwen88@gmail.com>
2025-03-03 15:27:58 +0800

    feat: update default image ref to aliyun

--- ros/run.sh
+++ ros/run.sh
@@ -10,5 +10,5 @@ docker run -it --rm \
     -e DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ \
     --privileged -v /usr/lib/x86_64-linux-gnu/:/usr/lib/x86_64-linux-gnu/ \
     ${MOUNT_ARGS} \
-    ${IMAGE_REF:-"sim-runtime"} \
+    ${IMAGE_REF:-"registry.cn-shanghai.aliyuncs.com/discover-robotics/simulator:full"} \
     python3 ros/play.py $@
