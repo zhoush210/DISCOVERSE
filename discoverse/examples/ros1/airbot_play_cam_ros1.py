51c99c7a4bdc36fb148641a638d787f7ebeb6ec2
Haizhou Ge <1352674740@qq.com>
2025-03-25 19:54:46 +0800

    encoding images as bgr8

--- discoverse/examples/ros1/airbot_play_cam_ros1.py
+++ discoverse/examples/ros1/airbot_play_cam_ros1.py
@@ -76,7 +76,7 @@ class AirbotPlayCam(AirbotPlayBase):
         if self.obs is None:
             return
         image = self.obs["img"][0]
-        self.image_puber.publish(cv_bridge.CvBridge().cv2_to_imgmsg(image[:, :, ::-1]))
+        self.image_puber.publish(cv_bridge.CvBridge().cv2_to_imgmsg(image[:, :, ::-1], "rgb8"))
         info = CameraInfo()
         info.header.stamp = rospy.Time.now()
         h, w, _ = image.shape
