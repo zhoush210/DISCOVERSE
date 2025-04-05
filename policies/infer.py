a4d0c283c7c8faa090d5dc3d7695578521c6d27c
Haizhou Ge <1352674740@qq.com>
2025-04-05 15:48:51 +0800

    udpate act and change command to python3

--- policies/infer.py
+++ policies/infer.py
@@ -30,9 +30,9 @@ if __name__ == "__main__":
 
     args = sys.argv[2:]
     if policy == "act":
-        command = f"cd policies/act && python policy_evaluate.py -res mujoco "
+        command = f"cd policies/act && python3 policy_evaluate.py -res mujoco "
     elif policy == "dp":
-        command = f"cd policies/dp && python train_eval.py "
+        command = f"cd policies/dp && python3 train_eval.py "
     else:
         raise NotImplementedError(f"Policy {policy} is not implemented.")
 
