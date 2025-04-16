1c90d44b424a6b3f5081c0200d41d0aa4aaf39f5
zhoush <zhoushihui210@foxmail.com>
2025-04-16 08:25:01 +0000

    add ros2 rf

--- discoverse/examples/ros2/mmk2_ros2.py
+++ discoverse/examples/ros2/mmk2_ros2.py
@@ -1,16 +1,19 @@
 import threading
 import numpy as np
+from scipy.spatial.transform import Rotation
 
 import rclpy
+import tf2_ros
 from rclpy.node import Node
 from cv_bridge import CvBridge
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import TransformStamped
 from sensor_msgs.msg import Image, CameraInfo, JointState, Imu, PointCloud2
 
 from discoverse.envs.mmk2_base import MMK2Base, MMK2Cfg
-from discoverse.utils import PIDarray, camera2k
+from discoverse.utils import PIDarray, camera2k, get_site_tmat
 
 class MMK2ROS2(MMK2Base, Node):
     target_control = np.zeros(19)
@@ -33,6 +36,9 @@ class MMK2ROS2(MMK2Base, Node):
             integrator_maxs=np.array([5.0, 5.0]),
         )
 
+        self.init_topic_publisher()
+        self.init_topic_subscriber()
+
         if self.config.lidar_s2_sim:
             from mujoco_lidar.lidar_wrapper import MjLidarWrapper
             from mujoco_lidar.scan_gen import create_lidar_single_line
@@ -43,11 +49,13 @@ class MMK2ROS2(MMK2Base, Node):
 
             self.lidar_s2 = MjLidarWrapper(self.mj_model, self.mj_data, site_name=self.lidar_frame_id)
             self.lidar_s2.get_lidar_points(self.rays_phi, self.rays_theta, self.mj_data)
-
-        self.init_topic_publisher()
-        self.init_topic_subscriber()
+    
+            self.static_broadcaster = tf2_ros.StaticTransformBroadcaster(self)
+            self.publish_static_transform(header_frame_id='mmk2_base', child_frame_id=self.lidar_frame_id)
 
     def init_topic_publisher(self):
+        self.tf_broadcaster = tf2_ros.TransformBroadcaster(self)
+
         # rostopic joint state
         self.joint_state_puber = self.create_publisher(JointState, '/mmk2/joint_states', 5)
         self.joint_state = JointState()
@@ -69,7 +77,7 @@ class MMK2ROS2(MMK2Base, Node):
         self.odom_puber = self.create_publisher(Odometry, '/mmk2/odom', 5)
         self.odom_msg = Odometry()
         self.odom_msg.header.frame_id = "odom"
-        self.odom_msg.child_frame_id = "mmk2_footprint"
+        self.odom_msg.child_frame_id = "mmk2_base"
 
         # image
         self.bridge = CvBridge()
@@ -172,6 +180,28 @@ class MMK2ROS2(MMK2Base, Node):
         self.mj_data.ctrl[:2] = np.clip(wheel_force, self.mj_model.actuator_ctrlrange[:2,0], self.mj_model.actuator_ctrlrange[:2,1])
         self.mj_data.ctrl[2:self.njctrl] = np.clip(action[2:self.njctrl], self.mj_model.actuator_ctrlrange[2:self.njctrl,0], self.mj_model.actuator_ctrlrange[2:self.njctrl,1])
 
+    def publish_static_transform(self, header_frame_id, child_frame_id):
+        stfs_msg = TransformStamped()
+        stfs_msg.header.stamp = self.get_clock().now().to_msg()
+        stfs_msg.header.frame_id = header_frame_id
+        stfs_msg.child_frame_id = child_frame_id
+
+        tmat_base = get_site_tmat(self.mj_data, header_frame_id)
+        tmat_child = get_site_tmat(self.mj_data, child_frame_id)
+        tmat_trans = np.linalg.inv(tmat_base) @ tmat_child
+        
+        stfs_msg.transform.translation.x = tmat_trans[0, 3]
+        stfs_msg.transform.translation.y = tmat_trans[1, 3]
+        stfs_msg.transform.translation.z = tmat_trans[2, 3]
+
+        quat = Rotation.from_matrix(tmat_trans[:3, :3]).as_quat()
+        stfs_msg.transform.rotation.x = quat[0]
+        stfs_msg.transform.rotation.y = quat[1]
+        stfs_msg.transform.rotation.z = quat[2]
+        stfs_msg.transform.rotation.w = quat[3]
+
+        self.static_broadcaster.sendTransform(stfs_msg)
+
     def thread_publidartopic(self, freq=12):
         if not self.config.lidar_s2_sim:
             return
@@ -211,6 +241,19 @@ class MMK2ROS2(MMK2Base, Node):
             self.odom_msg.twist.twist.angular.y = self.sensor_base_gyro[1]
             self.odom_msg.twist.twist.angular.z = self.sensor_base_gyro[2]
             self.odom_puber.publish(self.odom_msg)
+            
+            trans_msg = TransformStamped()
+            trans_msg.header.stamp = time_stamp
+            trans_msg.header.frame_id = "odom"
+            trans_msg.child_frame_id = "mmk2_base"
+            trans_msg.transform.translation.x = self.sensor_base_position[0]
+            trans_msg.transform.translation.y = self.sensor_base_position[1]
+            trans_msg.transform.translation.z = self.sensor_base_position[2]
+            trans_msg.transform.rotation.w = self.sensor_base_orientation[0]
+            trans_msg.transform.rotation.x = self.sensor_base_orientation[1]
+            trans_msg.transform.rotation.y = self.sensor_base_orientation[2]
+            trans_msg.transform.rotation.z = self.sensor_base_orientation[3]            
+            self.tf_broadcaster.sendTransform(trans_msg)
 
             self.imu_msg.header.stamp = time_stamp
             self.imu_msg.orientation.w = self.sensor_base_orientation[0]
